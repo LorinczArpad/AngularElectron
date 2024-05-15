b410499f096aa3039278e3459fde140cd8309996
lorincz.arpad <lorincz.dudu.arpad@gmail.com>
2024-05-15 11:18:11 +0200

    [M] ChandleChart with apexChart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MenubarModule } from 'primeng/menubar';
+import { CardModule } from 'primeng/card';
+import { NgApexchartsModule } from "ng-apexcharts";
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule,
+    CardModule,
+    NgApexchartsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+ }
